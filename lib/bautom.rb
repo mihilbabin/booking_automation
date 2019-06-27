96be4f1f6597c0b4eeb641b1937ebf8a9ef4c5ef
Michael Babin <iamsuperman@ukr.net>
2019-06-27 19:18:53 +0300

    added initialization

--- lib/bautom.rb
+++ lib/bautom.rb
@@ -2,5 +2,12 @@ require "bautom/version"
 
 module Bautom
   class Error < StandardError; end
-  # Your code goes here...
+  API_ENDPOINT = 'https://manage.BookingAutomation.com/api'.freeze
+
+  class Client
+    attr_accessor :auth_token
+    def initialize(auth_token)
+      @auth_token = auth_token
+    end
+  end
 end
